f7ad81d04d37dc40f97d947408a47f86663a0d20
mukuru-bless <blessing.chiura@mukuru.com>
2019-04-07 23:20:43 +0200

    update project structure and syntax as well as file generation method and .gitignore file

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import csv
-import random
+import random, os
 
 MY_CSV = "food_coded_temp.csv"
 TRAINING_SET = "train.csv"
@@ -7,64 +7,65 @@ TEST_SET = "test.csv"
 
 
 def open_user_file():
-	# open csv file and write each row into array (students)
-	with open(MY_CSV) as csv_data:
-		data = csv.DictReader(csv_data)
-		count = 0
-		students =[]
-		for row in data:
-			students.append(row)
-			count += 1 #keep track of entries into array
-	print("\n\n\nNumber of datasets is: {}".format(count))
+    # open csv file and write each row into array (students)
+    with open(MY_CSV) as csv_data:
+        data = csv.DictReader(csv_data)
+        count = 0
+        students =[]
+        for row in data:
+            students.append(row)
+            count += 1  # keep track of entries into array
 
-	return students
+        print("\n\n\nNumber of datasets is: {}".format(count))
+    return students
+
 
 def split_data(data):
-	#data will be split into 80% train and 20% test
-	x_ = 0.8 * len(data)
-	x_ = int(x_)
-	print ("\n\nNumber of training set: {}".format(x_) )
+    # data will be split into 80% train and 20% test
+    x_ = 0.8 * len(data)
+    x_ = int(x_)
+    print("\n\nNumber of training set: {}".format(x_) )
 
-	x_train = []
-	#training data is selected at random form array
-	for row in range(x_):
-		#pick random number
-		index = random.randint(0, len(data) - 1)
-		#pick data at randomly selected place 
-		get_data = data[index]
-		#add data selected randomly to new training set array
-		x_train.append(get_data)
-		#delete selected data from original array
-		del data[index]
+    x_train = []
+    # training data is selected at random form array
+    for row in range(x_):
+        # pick random number
+        index = random.randint(0, len(data) - 1)
+        # pick data at randomly selected place
+        get_data = data[index]
+        # add data selected randomly to new training set array
+        x_train.append(get_data)
+        # delete selected data from original array
+        del data[index]
 
-	print("Training set has {} students.\n".format(len(x_train)))
+    print("Training set has {} students.\n".format(len(x_train)))
 
-	print("\nTest set is {} students.".format(len(data)))
+    print("\nTest set is {} students.".format(len(data)))
 
-	heading = ["GPA","Gender","breakfast","calories_chicken","calories_day","calories_scone","coffee",\
-			"comfort_food","comfort_food_reasons","comfort_food_reasons_coded","cook","comfort_food_reasons_coded",\
-			"cuisine","diet_current","diet_current_coded","drink","eating_changes","eating_changes_coded",\
-			"eating_changes_coded1","eating_out","employment","ethnic_food","exercise","father_education",\
-			"father_profession","fav_cuisine","fav_cuisine_coded","fav_food","food_childhood","fries",\
-			"fruit_day","grade_level","greek_food","healthy_feeling","healthy_meal","ideal_diet",\
-			"ideal_diet_coded","income","indian_food","italian_food","life_rewarding","marital_status",\
-			"meals_dinner_friend","mother_education","mother_profession","nutritional_check","on_off_campus",\
-			"parents_cook","pay_meal_out","persian_food","self_perception_weight","soup","sports","thai_food","tortilla_calories",\
-			"turkey_calories","type_sports","veggies_day","vitamins","waffle_calories","weight"]
+    heading = ["GPA","Gender","breakfast","calories_chicken","calories_day","calories_scone","coffee",
+                "comfort_food","comfort_food_reasons","comfort_food_reasons_coded","cook","comfort_food_reasons_coded",
+                "cuisine","diet_current","diet_current_coded","drink","eating_changes","eating_changes_coded",
+                "eating_changes_coded1","eating_out","employment","ethnic_food","exercise","father_education",
+                "father_profession","fav_cuisine","fav_cuisine_coded","fav_food","food_childhood","fries",
+                "fruit_day","grade_level","greek_food","healthy_feeling","healthy_meal","ideal_diet",
+                "ideal_diet_coded","income","indian_food","italian_food","life_rewarding","marital_status",
+                "meals_dinner_friend","mother_education","mother_profession","nutritional_check","on_off_campus",
+                "parents_cook","pay_meal_out","persian_food","self_perception_weight","soup","sports","thai_food","tortilla_calories",
+                "turkey_calories","type_sports","veggies_day","vitamins","waffle_calories","weight"]
 
-	with open (TRAINING_SET, "w") as training:
-		csv_train = csv.DictWriter(training, fieldnames=heading)
+    with open(TRAINING_SET, "w") as training:
+        csv_train = csv.DictWriter(training, fieldnames=heading)
+        csv_train.writeheader()
 
-		csv_train.writeheader()
+        for row in x_train:
+            csv_train.writerow(row)
 
-		for row in x_train:
-			csv_train.writerow(row)
+    with open(TEST_SET, 'w') as test:
+        csv_test = csv.DictWriter(test, fieldnames=heading)
+        csv_test.writeheader()
 
-	with open(TEST_SET, 'w') as test:
-		csv_test = csv.DictWriter(test, fieldnames=heading)
-		csv_test.writeheader()
+        for row in data:
+            csv_test.writerow(row)
 
-		for row in data:
-			csv_test.writerow(row)
 
 split_data(open_user_file())
--- user_interface.py
+++ user_interface.py
@@ -8,49 +8,53 @@ from tkinter import (
 import api
 import json
 
-class MainWindow():
+
+class MainWindow:
 
     def __init__(self, root):
         super().__init__()
 
         self.root = root
 
-        #this is the user menu, loads everytime on launch
-        Button(self.root, text = "View Restaurants Types", 
-                height=4, width=16, 
-                padx=1, pady=1, 
-                command=viewingRestaurantTypesWindow)\
-            .place(x = 150, y = 90)
+        # this is the user menu, loads every time on launch
+        Button(self.root, text="View Restaurants Types",
+               height=4, width=16,
+               padx=1, pady=1,
+               command=viewing_restaurant_types_window)\
+            .place(x=150, y=90)
 
-        Button(self.root, text = "View Restaurant", 
-                height=4, width=16, 
-                padx=1, pady=1,
-                command=viewingRestaurantWindow)\
-            .place(x = 150, y = 180)
+        Button(self.root, text="View Restaurant",
+               height=4, width=16,
+               padx=1, pady=1,
+               command=viewing_restaurant_window)\
+            .place(x=150, y=180)
 
-        Button(self.root, text = "View Meal Types", 
-                height=4, width=16, 
-                padx=1, pady=1, 
-                command=viewingMealTypeWindow)\
-            .place(x = 150, y = 270)
+        Button(self.root, text="View Meal Types",
+               height=4, width=16,
+               padx=1, pady=1,
+               command=viewing_meal_type_window)\
+            .place(x=150, y=270)
 
-        Button(self.root, text = "Get Recommendation", 
-                height=4, width=16, 
+        Button(self.root, text="Get Recommendation",
+                height=4, width=16,
                 padx=1, pady=1,
                 command=GetRecommendation)\
-        .place(x = 150, y = 360)
+            .place(x=150, y=360)
 
         Button(self.root, text='Quit',
                 width=16,
                 fg='red', 
                 command=self.root.destroy)\
-        .place(x = 150,y = 440)
-        
-#re-useable method to pressback and destroy window
-def pressBackButton(window):
+            .place(x=150, y=440)
+
+
+# reusable method to press-back and destroy window
+def press_back_button(window):
     return Button(window, text="Back", command=window.destroy)
-#------------------------------------------------------------------
-class ViewRestaurantTypeWindow():
+# ------------------------------------------------------------------
+
+
+class ViewRestaurantTypeWindow:
     
     def __init__(self):
         data = json.loads(api.get_establishments())
@@ -63,12 +67,13 @@ class ViewRestaurantTypeWindow():
 
         Button(window, text="Back", command=window.destroy).pack()
 
-#------------------------------------------------------------------
+# ------------------------------------------------------------------
 
-class ViewRestaurantWindow():
+
+class ViewRestaurantWindow:
     
     def __init__(self):
-        data = json.loads(api. get_rest())
+        data = json.loads(api.get_rest())
         window = Tk()
         window.title("View Restaurants")
         listbox = Listbox(window, width=30, height=15)
@@ -78,9 +83,10 @@ class ViewRestaurantWindow():
 
         Button(window, text="Back", command=window.destroy).pack()
 
-#------------------------------------------------------------------
+# ------------------------------------------------------------------
 
-class ViewMealTypesWindow():
+
+class ViewMealTypesWindow:
 
     def __init__(self):
         window = Tk()
@@ -94,92 +100,104 @@ class ViewMealTypesWindow():
 
         Button(window, text="Back", command=window.destroy).pack()
 
-#------------------------------------------------------------------
-class GetRecommendation():
+# ------------------------------------------------------------------
+
+
+class GetRecommendation:
     def __init__(self):
         window = Tk()
         window.geometry('400x200')
         window.title("Get Recommendations")
 
-        Button(window, text="I have a user id", 
-                        height=2, width=16,
-                        command=getUserId)\
-                .place(x = 40, y = 70)
-        Button(window, text="I want a user id", 
-                        height=2, width=16,
-                        command=createUserId)\
-                .place(x = 210, y = 70)
+        Button(window, text="I have a user id",
+               height=2, width=16,
+               command=get_user_id)\
+            .place(x=40, y=70)
+        Button(window, text="I want a user id",
+               height=2, width=16,
+               command=create_user_id)\
+            .place(x=210, y=70)
         Button(window, text="Back", command=window.destroy)\
-                .place(x = 175, y = 150)
+            .place(x=175, y=150)
 
-def viewingRestaurantTypesWindow():
+
+def viewing_restaurant_types_window():
     ViewRestaurantTypeWindow()
 
-def viewingRestaurantWindow():
+
+def viewing_restaurant_window():
     ViewRestaurantWindow()
 
-def viewingMealTypeWindow():
+
+def viewing_meal_type_window():
     ViewMealTypesWindow()
 
-def getRecommendationMenu():
+
+def get_recommendation_menu():
     GetRecommendation()
 
-#------------------------------------------------------------------
-#to get user id
-class SignUp():
+# ------------------------------------------------------------------
+
+
+# to get user id
+class SignUp:
 
     def __init__(self):
         window = Tk()
         window.geometry("300x150")
         window.title("Sign Up")
 
-        Label(window, text="Full Name: ",width=10,font=(10)).place(x=20, y=40)
+        Label(window, text="Full Name: ",width=10,font=10).place(x=20, y=40)
 
         Entry(window).place(x=100, y=40)
 
-        Label(window, text="User Id: ",width=10,font=(10)).place(x=20, y=40)
+        Label(window, text="User Id: ",width=10,font=10).place(x=20, y=40)
 
         Entry(window).place(x=100, y=40)
 
         Button(window, text="Sign Up").place(x=120, y=70)
         Button(window, text="Back", command=window.destroy).place(x=180, y=70)
+# ------------------------------------------------------------------
 
 
-#------------------------------------------------------------------
-#look up user
-class LogIn():
+# look up user
+class LogIn:
     def __init__(self): 
         window = Tk()
         window.geometry("300x150")
         window.title("Search")
 
-        Label(window, text="User Id: ",width=10,font=(10)).place(x=20, y=40)
+        Label(window, text="User Id: ", width=10, font=10).place(x=20, y=40)
 
         Entry(window).place(x=100, y=40)
 
-        Label(window, text="User Id: ",width=10,font=(10)).place(x=20, y=40)
+        Label(window, text="User Id: ", width=10, font=10).place(x=20, y=40)
 
         Entry(window).place(x=100, y=40)
 
         Button(window, text="Search").place(x=120, y=70)
         Button(window, text="Back", command=window.destroy).place(x=180, y=70) 
 
-def createUserId():
+
+def create_user_id():
     SignUp()
 
-def getUserId():
+
+def get_user_id():
     LogIn()
 
-#------------------------------------------------------------------
-def launchApp():
+# ------------------------------------------------------------------
+
+
+def _launch():
     window = Tk()
     window.geometry("500x800")
     window.title("Restaurant Recommendation System")
-    Label(window, text = "Welcome to RRS"
+    Label(window, text="Welcome to RRS"
                 , width=20, font=("bold", 20))\
-    .place(x = 90, y = 53)
+        .place(x=90, y=53)
     MainWindow(window)
     window.mainloop()
 
 
-launchApp()
+_launch()
